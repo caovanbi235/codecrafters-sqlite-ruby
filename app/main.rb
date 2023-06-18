16c12f18269815196bfc580200350f07de739688
Cao Van Bi <cao.vanbi@moneyforward.co.jp>
2023-06-18 21:42:39 +0900

    add feature of print number of tables

--- app/main.rb
+++ app/main.rb
@@ -6,5 +6,9 @@ if command == ".dbinfo"
     database_file.seek(16)  # Skip the first 16 bytes of the header
     page_size = database_file.read(2).unpack("n")[0]
     puts "database page size: #{page_size}"
+
+    database_file.seek(26)
+    number_of_tables = database_file.read(4).unpack("n")[0]
+    puts "number of tables: #{number_of_tables}"
   end
 end
